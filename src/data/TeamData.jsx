a610bc7baeab8ec6ca11e00677488a5113f9ba39
Francisco Fernández <ffercal0712@g.educaand.es>
2026-05-11 09:09:00 +0200

    edit/add - escudo compactaciones

--- src/data/TeamData.jsx
+++ src/data/TeamData.jsx
@@ -1,38 +1,40 @@
-import unicajaShield from '../assets/escudos/unicaja2.png';
-import puertaOscuraShield from '../assets/escudos/cd_puerta_oscura.png';
-import salliverShield from '../assets/escudos/cb_salliver.png';
+import unicajaEscudo from '../assets/escudos/unicaja2.png';
+import puertaOscuraEscudo from '../assets/escudos/cd_puerta_oscura.png';
+import salliverEscudo from '../assets/escudos/cb_salliver.png';
 import oksapLogo from '../assets/logo-oksap.png';
 import cpMijasEscudo from '../assets/escudos/cp_mijas.png';
+import compactacionesEscudo from '../assets/escudos/compactaciones.png';
 
 const equipos = [
     {
         id: 1,
         slug: 'unicaja-master-55',
         equipo: 'UNICAJA MASTER +55',
-        escudo: unicajaShield
+        escudo: unicajaEscudo
     },
     {
         id: 2,
         slug: 'c-b-puerta-oscura-veteranos',
         equipo: 'C.B. PUERTA OSCURA VETERANOS',
-        escudo: puertaOscuraShield
+        escudo: puertaOscuraEscudo
     },
     {
         id: 3,
         slug: 'unicaja-master-65',
         equipo: 'UNICAJA MASTER +65',
-        escudo: unicajaShield
+        escudo: unicajaEscudo
     },
     {
         id: 4,
         slug: 'compactaciones-andalucia',
-        equipo: 'COMPACTACIONES ANDALUCÍA'
+        equipo: 'COMPACTACIONES ANDALUCÍA',
+        escudo: compactacionesEscudo
     },
     {
         id: 5,
         slug: 'unicaja-master-50',
         equipo: 'UNICAJA MASTER +50',
-        escudo: unicajaShield
+        escudo: unicajaEscudo
     },
     {
         id: 6,
@@ -51,7 +53,7 @@ const equipos = [
         id: 8,
         slug: 'c-b-salliver-blanco',
         equipo: 'C.B. SALLIVER BLANCO',
-        escudo: salliverShield
+        escudo: salliverEscudo
     }
 ];
 
